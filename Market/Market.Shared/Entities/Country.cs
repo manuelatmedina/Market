f09c5a360e0a20d47781f4aab3a01525dabfc17a
Manuela Tenganàn Medina <manuelamedinat@hotmail.com>
2023-08-30 14:00:28 -0500

    Creamos Entity Country

---
+++ Market/Market.Shared/Entities/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [Display(Name = "Pais")]
+        [MaxLength(100)]
+        [Required(ErrorMessage ="El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null;
+    }
+}
